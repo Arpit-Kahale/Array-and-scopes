bb62b9bdecfb4535b369bb54387c65f004aae976
Arpit Kahale <arpitkahalek13@gmail.com>
2026-01-09 05:32:49 -0800

    push Arraymethod.js file

---
+++ Arraymethod.js
@@ -0,0 +1,38 @@
+// push,pop,shift,unshift
+
+// push--If we wanted to add or push the element end of the array then we used the the push method.
+
+//pop--If we wanted to remove the elemment end of the arrray then we used the pop method.
+
+//shift--If we wanted to remove the element begning of the array then we used the shift methods
+
+// unshift--If we wanted to add or push the element to the begaing of the aaray then we used the un shift method.
+
+
+
+// 1.push method...
+
+let a=[1,3]
+a.push(5)
+console.log(a)
+
+//2. POP METHOD
+
+let b=[1,2,3]
+b.pop()
+console.log(b)
+
+// 3. Unshift method.
+
+const c=[1,2,3]
+c.unshift(3)
+console.log(c)
+
+//4.shift meethod 
+ const d=[1,2,3,4,5]
+ d.shift()
+ console.log(d)
+
+
+
+ 
